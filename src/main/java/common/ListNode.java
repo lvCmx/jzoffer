237c2094b71651486729d979a497d42d37c684d8
shixiaoling <shixiaoling_i@didichuxing.com>
2019-03-26 15:28:49 +0800

    删除多余文件

---
+++ src/main/java/common/ListNode.java
@@ -0,0 +1,10 @@
+package common;
+
+public class ListNode{
+    public int value;
+    public ListNode next;
+    ListNode(int value,ListNode next){
+        this.value=value;
+        this.next=next;
+    }
+}
---
+++ src/main/java/common/ListNodeUtil.java
@@ -0,0 +1,52 @@
+package common;
+
+/**
+ * 说明：一些公用的东西就写到这里面了
+ */
+public class ListNodeUtil {
+    //循环输出链表
+    public static void print(ListNode node){
+        while(null!=node){
+            System.out.println(node.key);
+            node=node.next;
+        }
+    }
+
+    // 生成一个链表，用于测试
+    public static ListNode createListNode(int [] arr){
+        if(null!=arr && arr.length>0){
+            ListNode result=new ListNode(-1,null);
+            ListNode root=result;
+            for(int i=0;i<arr.length;i++){
+                result.next=new ListNode(arr[i],null);
+                result=result.next;
+            }
+            return root.next;
+        }
+        return null;
+    }
+
+    public static ListNode createListNodeHasHead(int[] arr) {
+        if(null!=arr && arr.length>0){
+            ListNode result=new ListNode(-1,null);
+            ListNode root=result;
+            for(int i=0;i<arr.length;i++){
+                result.next=new ListNode(arr[i],null);
+                result=result.next;
+            }
+            return root;
+        }
+        return null;
+    }
+
+    public static void printNotHasHead(ListNode node) {
+        if(null==node || null==node.next){
+            return;
+        }
+        node=node.next;
+        while(null!=node){
+            System.out.println(node.key);
+            node=node.next;
+        }
+    }
+}
