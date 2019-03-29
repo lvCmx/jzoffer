ca4180b1839484d20bed36ad40bf6ead982da50c
shixiaoling <shixiaoling_i@didichuxing.com>
2019-03-29 11:17:02 +0800

    动态规划解决 连续子数组的最大和

--- src/main/java/第五章_优化时间和空间效率/连续子数组的最大和.java
+++ src/main/java/第五章_优化时间和空间效率/连续子数组的最大和.java
@@ -5,7 +5,7 @@ package 第五章_优化时间和空间效率;
  */
 public class 连续子数组的最大和 {
     public static void main(String[] args) {
-        int[] arr={1,2,-4,5,7,-8,4,-3,6,-8};
+        int[] arr = {1, 2, -4, 5, 7, -8, 4, -3, 6, -8};
         int greatestSumOfSubArray = findGreatestSumOfSubArray(arr);
         System.out.println(greatestSumOfSubArray);
 
@@ -14,20 +14,20 @@ public class 连续子数组的最大和 {
 
     }
 
-    public static int findGreatestSumOfSubArray(int[] arr){
-        if(null==arr || arr.length<=0){
+    public static int findGreatestSumOfSubArray(int[] arr) {
+        if (null == arr || arr.length <= 0) {
             return 0;
         }
-        int sum=0;
-        int greatestSum=0;
-        for(int i=0;i<arr.length;i++){
-            if(sum<=0){
-                sum=arr[i];
-            }else{
-                sum+=arr[i];
+        int sum = 0;
+        int greatestSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (sum <= 0) {
+                sum = arr[i];
+            } else {
+                sum += arr[i];
             }
-            if(sum>greatestSum){
-                greatestSum=sum;
+            if (sum > greatestSum) {
+                greatestSum = sum;
             }
         }
         return greatestSum;
@@ -38,17 +38,16 @@ public class 连续子数组的最大和 {
      * 动态规划思路：对于第i项来说，要么选择第i项+之前的和，要么只选择第i项抛弃之前的和
      * 也就是
      * DP[i] = max(dp[i-1]+num[i] , num[i])
+     *
      * @param array
      * @return
      */
     public static int findSumOfSubArray(int[] array) {
-
-        if (array == null || array.length == 0) {
-            return 0;
+        if (array == null || array.length <= 0) {
+            return -1;
         }
         int sum = array[0];
         int max = array[0];
-
         for (int i = 1; i < array.length; i++) {
             sum = Math.max(sum + array[i], array[i]);
             if (sum > max) {
@@ -56,7 +55,5 @@ public class 连续子数组的最大和 {
             }
         }
         return max;
-
     }
-
 }
